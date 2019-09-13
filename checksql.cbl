df29bed57344d7cfb7c91407c0bc0e1621a9de35
wahidaleena <34679809+wahidaleena@users.noreply.github.com>
2019-09-13 12:23:38 +0530

    Update checksql.cbl

--- checksql.cbl
+++ checksql.cbl
@@ -1,1 +1,25 @@
+ IDENTIFICATION DIVISION.
+       PROGRAM-ID.    TEST DB2.
+      *SECURITY.      OPERACTION, REVISION, AND DISTRIBUTION
+      *            OF THIS PROGRAM BY WRITTEN AUTHORIZATION
+      *            OF THE ABOVE INSTALLACTION ONLY.
+      *DATE-WRITTEN.  09/12/19.
+      *DATE-COMPLETED.
+      **************************CC109**********************************
+       
+       PROCEDURE DIVISION.
+       0000-INITIALIZE-PARA.
+          
+        EXEC SQL
+            INSERT
+            INTO     WZIP
+            (POSTAL_ZIP_CD, GROWING_REGION_CD)
+            VALUES (:ZIP-POSTAL(SEG-INDX), :GREDIT)
+
+        END-EXEC.
+     
+       6000-FINAL-COUNT.
+          CLOSE INFILE
+                OUT.
+          DISPLAY "-----------------------------------------------------".
 
